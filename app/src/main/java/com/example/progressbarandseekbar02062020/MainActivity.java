f19227b6806e636081378d8578ac0c03c6115451
phatdroid94 <phatdroid94@gmail.com>
2020-07-07 19:31:37 +0700

    count downtimer

--- app/src/main/java/com/example/progressbarandseekbar02062020/MainActivity.java
+++ app/src/main/java/com/example/progressbarandseekbar02062020/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.progressbarandseekbar02062020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView mTvCurrent,mTvTotal;
+    ProgressBar mProgress;
+    int count = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTvCurrent = findViewById(R.id.textviewCurrentLoading);
+        mTvTotal = findViewById(R.id.textviewTotal);
+        mProgress = findViewById(R.id.progressbar);
+
+        // CountDownTimer
+
+        CountDownTimer countDownTimer = new CountDownTimer(5000,2000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                Log.d("BBB",millisUntilFinished + "");
+            }
+
+            @Override
+            public void onFinish() {
+//                Log.d("BBB",count + "");
+            }
+        };
+        countDownTimer.start();
     }
 }
