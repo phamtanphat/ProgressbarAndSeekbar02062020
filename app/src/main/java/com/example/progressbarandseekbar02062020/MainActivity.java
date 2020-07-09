dd0f365699f98756a74b35e88a733a3d229abb25
phatdroid94 <phatdroid94@gmail.com>
2020-07-09 19:09:27 +0700

    update id

--- app/src/main/java/com/example/progressbarandseekbar02062020/MainActivity.java
+++ app/src/main/java/com/example/progressbarandseekbar02062020/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
