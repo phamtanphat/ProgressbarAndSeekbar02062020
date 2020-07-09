b4d25424d168eaa2973a577a12c33a45dbcffa55
phatdroid94 <phatdroid94@gmail.com>
2020-07-09 19:04:59 +0700

    ui color

--- app/src/main/java/com/example/progressbarandseekbar02062020/MainActivity.java
+++ app/src/main/java/com/example/progressbarandseekbar02062020/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
+import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,39 +14,11 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView mTvCurrent,mTvTotal;
-    ProgressBar mProgress;
-    Random mRandom;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mTvCurrent = findViewById(R.id.textviewCurrentLoading);
-        mTvTotal = findViewById(R.id.textviewTotal);
-        mProgress = findViewById(R.id.progressbar);
-
-        mRandom = new Random();
-        // CountDownTimer
-        final CountDownTimer countDownTimer = new CountDownTimer(100000,1000) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-                if (mProgress.getProgress() >= 100){
-                    onFinish();
-                }else{
-                    mProgress.setProgress(mProgress.getProgress() + mRandom.nextInt(11));
-                    mTvCurrent.setText(mProgress.getProgress() + "%");
-                    mTvTotal.setText(String.format("%d/%d",mProgress.getProgress(),100));
-                }
-            }
-
-            @Override
-            public void onFinish() {
-                Toast.makeText(MainActivity.this, "Ket thuc", Toast.LENGTH_SHORT).show();
-                this.cancel();
-            }
-        };
-        countDownTimer.start();
 
 
     }
